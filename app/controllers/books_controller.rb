47da25afd3609d5373e48c75da3ad3e7ed820098
minami-taiga <373735656t@gmail.com>
2019-12-23 09:54:49 +0000

    first commit finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+	def new
+    end
+    def create
+    	    @book = Book.new(book_params)
+    	    @book.user_id = current_user.id
+     if @book.save
+     redirect_to book_path(@book.id), notice: "You have creatad book successfully."
+     else
+      @books = Book.all
+      @user = current_user
+      render :index
+     end
+    end
+    def index
+        @books = Book.all
+      	@book = Book.new
+        @user = current_user
+    end
+    def show
+    	@book = Book.find(params[:id])
+        @new_book = Book.new
+        @user = @book.user
+    end
+    def destroy
+      @book = Book.find(params[:id])
+      @book.destroy
+      redirect_to books_path
+    end
+    
+    def edit
+        @book = Book.find(params[:id])
+        if @book.user.id != current_user.id
+            redirect_to books_path
+        end
+    end
+     def update
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+        redirect_to book_path(@book.id), notice: "You have updated book successfully."
+      else
+        render :edit
+        end
+     end
+
+     private
+ def book_params
+ 	params.require(:book).permit(:title,:body)
+ end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'b69ff43339429c71e7c31c8ae10602c5270b40ea1925859f4079ce451b68ff449cf721eaaa57ba59317a51082fa9af8e447b441058855963709e4fc84f46c008'
